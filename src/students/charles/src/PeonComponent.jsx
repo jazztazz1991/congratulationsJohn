e8b2a6e37cd0ab3db9a6d0342d70553657f3a4b4
frenzie24 <kevincharlesgross@gmail.com>
2024-06-08 17:14:26 -0400

    fixed shit, improved shit, you get 3 peones to control now.  s p and spacebar pause the game and mute

--- src/students/charles/src/PeonComponent.jsx
+++ src/students/charles/src/PeonComponent.jsx
@@ -2,9 +2,8 @@
 import './App.css'
 import Canvas from './components/Canvas.jsx'
 import Peon from './peon.js'
-import VideoBackground from './components/VideoBackground.jsx'
-import WC3UI from './components/wc3ui.jsx'
-import './css/video.css'
+import '../src/css/video.css'
+import '../src/css/style.css'
 function PeonComponent() {
 
   // const audioRef = useRef(null);
@@ -14,11 +13,28 @@ function PeonComponent() {
   let debugMode = false;
   const width = window.innerWidth;
   const height = 250;
+  const topUI = new Image();
+  const botUI = new Image();
+  const bg = new Image();
+
+  topUI.src = "./src/students/charles/src/assets/topui.png";
+  botUI.src = "./src/students/charles/src/assets/bottomui.png";
+  bg.src = "https://cdna.artstation.com/p/marketplace/presentation_assets/000/500/478/large/file.jpg?1598966424"
+  let music;
+  let firstRun = true;
   const draw = context => {
+
+
     if (!peons) {
       peons = [];
       console.log('pushing new peon')
+      let count = 0;
       peons.push(new Peon(context, debugMode));
+      let peonInterval = setInterval(()=>
+        {peons.push(new Peon(context, debugMode));
+          if(++count == 2)
+            clearInterval(peonInterval);
+        }, 5000);
 
 
       setInterval(() => {
@@ -30,13 +46,20 @@ function PeonComponent() {
 
         //context.fillText="black"
         const gratz = "GGWP, man!"
+
         context.strokeText(gratz, width / 2 - context.measureText(gratz).width / 4, height - 50);
         context.textAlign = 'center';
         context.fillText(gratz, width / 2 - context.measureText(gratz).width / 4, height - 50);
+
+        context.drawImage(bg, 0, (height - bg.height) / 2)
         peons.forEach(peon => {
           peon.update();
-
+          if (peon.getDebugMode() != debugMode) peon.setDebugMode(debugMode);
         });
+        context.drawImage(topUI, (width - topUI.width) / 2, 0);
+        context.drawImage(botUI, (width - botUI.width) / 2, 250 - botUI.height);
+
+
       }, 100)
 
       console.log(`current peon count: ${peons.length}`);
@@ -47,30 +70,58 @@ function PeonComponent() {
 
 
   function handleCanvasClick(e) {
+    if (firstRun) {
+      music = new Audio("./src/students/charles/src/assets/sounds/Orc02.mp3");
+      music.addEventListener("canplaythrough", (ev) => {
+        console.count('loading audtio')
+        music.play()
+
+      });
+      music.addEventListener('ended', function () {
+        music.currentTime = 0;
+        music.play();
+      }, false);
+      firstRun = false;
+    }
     e.preventDefault();
     const x = e.clientX;
     const y = e.clientY;
-    peons.find(peon => {
-      peon.isLoaded() ? peon.checkClicked(x, y) : -1;
-      peon.setActive(true);
-      peon.setDebugMode(debugMode);
+    peons.map(peon => {
+
+      if (peon.isLoaded()) {
+        peon.setActive(peon.checkClicked(x, y))
+      } else peon.setActive(true);
+      if(peon.getActive()) music.play();
+     // else music.pause();
+      // peon.setActive(true);
+
     })
   }
 
   const onKeyDown = (ev) => {
     ev.preventDefault();
     if (ev.key == 'd') debugMode = !debugMode;
+    if (ev.key == 's'|| 'p' || 'spacebar' ) {
+
+      peons.forEach(peon => {
+        peon.setActive(!peon.getActive())
+        if(peon.getActive()) music.play();
+        else music.pause();
+      })
+    }
+
+
+
   }
 
   return (
-    <div className='container flex justify-center w-full' tabIndex={0} onKeyDown={ (ev)=>{ onKeyDown(ev)} }>
+    <div className={`container flex flex-row flex-wrap justify-center align-center w-full h-250`} tabIndex={0} onKeyDown={(ev) => { onKeyDown(ev) }}>
 
 
-      <WC3UI> </WC3UI>
-      <VideoBackground ></VideoBackground>
+
+
 
       <Canvas handleClick={handleCanvasClick} draw={draw} height={height} width={width} on />
-
 
     </div>
   )
